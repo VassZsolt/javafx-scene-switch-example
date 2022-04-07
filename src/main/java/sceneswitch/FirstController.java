582a648135212f4ea4381affeab84238080416b8
Péter Jeszenszky <jeszenszky.peter@inf.unideb.hu>
2022-04-07 21:07:53 +0200

    Initial import

---
+++ src/main/java/sceneswitch/FirstController.java
@@ -0,0 +1,23 @@
+package sceneswitch;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FirstController {
+
+    @FXML
+    private void switchScene(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/second.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+}
